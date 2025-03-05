f052f10b7c8692921e326e46c89f26f79f13ce93
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2025-03-05 18:58:08 -0300

    Finalização, documentação e reponsividade

--- script/script.js
+++ script/script.js
@@ -1,11 +1,34 @@
-function load(){
+function updateClock(){ //função feita para atualizar o relogio
 
 var data = new Date()
-var hora = data.getHours()
+var hour = data.getHours()
+var minutes = String(data.getMinutes()).padStart(2, '0'); //Tipo String porque o padStart deixa o número com um 0 a esquerda, e isso só pode ser feito por string
+var seconds = String(data.getSeconds()).padStart(2, '0');
+
+
+var image = window.document.getElementById('img')
 var clock = window.document.getElementById('clock')
+var message = document.getElementById('message')
 
-clock.innerHTML = hora
-
-
-
+if (hour > 5 && hour <= 12){
+    image.src = 'imagens/manha.png'
+    document.body.style.background = '#A3B63C'
+    message.innerText = 'Bom Dia!!'
+    
+} else if (hour > 12 && hour <=17){
+    image.src = 'imagens/tarde.png'
+    document.body.style.background = '#FEFD9C'
+    message.innerText = 'Boa Tarde!!'
+} else {
+    image.src = 'imagens/noite.png'
+    document.body.style.background = '#2D5E87'
 }
+
+clock.innerHTML = `${hour} ${minutes} ${seconds}`
+}
+
+function startClock(){
+    updateClock();
+    setInterval(updateClock, 1000);
+}
+
